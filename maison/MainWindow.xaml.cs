96b1a3d2779af614b8a64081f5c335a0aab3c70d
calliouxes <92147165+EliseeLeydier@users.noreply.github.com>
2023-04-19 13:10:04 +0200

    Add files via upload

---
+++ maison/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace maison
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Point lastPoint;
+        private Piece piece;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AjouterGrille();
+            piece = new Piece(canvas);
+        }
+
+        private void AjouterGrille()
+        {
+            // redessiner la grille lorsque la fenêtre est redimensionnée
+            this.SizeChanged += (s, e) =>
+            {
+                canvas.Children.Clear();
+                // dessiner des lignes horizontales
+                for (int i = 0; i < (this.ActualHeight / 20); i++)
+                {
+                    Line line = new Line();
+                    line.X1 = 0;
+                    line.Y1 = i * 20;
+                    line.X2 = canvas.ActualWidth;
+                    line.Y2 = i * 20;
+                    line.Stroke = Brushes.LightBlue;
+                    line.StrokeThickness = 1;
+                    canvas.Children.Add(line);
+                }
+
+                // dessiner des lignes verticales
+                for (int i = 0; i < (this.ActualWidth / 20); i++)
+                {
+                    Line line = new Line();
+                    line.X1 = i * 20;
+                    line.Y1 = 0;
+                    line.X2 = i * 20;
+                    line.Y2 = canvas.ActualHeight;
+                    line.Stroke = Brushes.LightBlue;
+                    line.StrokeThickness = 1;
+                    canvas.Children.Add(line);
+                }
+            };
+        }
+
+        /*private void MyControl_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                points[0].X = 0;
+                points[0].Y = 0;
+            }
+        }*/
+
+        // Bouton de la souris pressé
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            double x = Math.Round(e.GetPosition(canvas).X / 20.0) * 20.0;
+            double y = Math.Round(e.GetPosition(canvas).Y / 20.0) * 20.0;
+            piece.DessinCarre(x, y);
+        }
+
+    }
+}
---
+++ maison/Piece.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace maison
+{
+    internal class Piece
+    {
+        public Point[] Points { get; private set; }
+        public Canvas Canvas { get; private set; }
+
+        public Piece(Canvas canvas)
+        {
+            Points = new Point[4];
+            InitializePoints();
+            Canvas = canvas;
+        }
+
+        public void InitializePoints()
+        {
+            for (int i = 0; i < Points.Length; i++)
+            {
+                Points[i] = new Point(0, 0);
+            }
+        }
+
+        public void DessinCarre(double x, double y)
+        {
+            // Si c'est la première fois que l'on appuie
+            if (Points[0].X == 0 && Points[0].Y == 0)
+            {
+                Points[0].X = x;
+                Points[0].Y = y;
+                creerRond(Points[0].X, Points[0].Y);
+            }
+            else // Si ce n'est pas la première fois
+            {
+                creerRond(x, y);
+                creerRond(Points[0].X, y);
+                creerRond(x, Points[0].Y);
+
+                Points[1] = new Point(Points[0].X, y);
+                Points[2] = new Point(x, Points[0].Y);
+                Points[3] = new Point(x, y);
+
+                Canvas.Children.Add(creerLine(Points[0], Points[1]));
+                Canvas.Children.Add(creerLine(Points[1], Points[3]));
+                Canvas.Children.Add(creerLine(Points[3], Points[2]));
+                Canvas.Children.Add(creerLine(Points[2], Points[0]));
+
+                InitializePoints();
+            }
+        }
+        private Line creerLine(Point point1, Point point2)
+        {
+            Line line = new Line
+            {
+                Stroke = Brushes.Black,
+                X1 = point1.X,
+                Y1 = point1.Y,
+                X2 = point2.X,
+                Y2 = point2.Y
+            };
+            return line;
+        }
+
+        private void creerRond(double x, double y)
+        {
+            Ellipse circle = new Ellipse
+            {
+                Width = 10,
+                Height = 10,
+                Fill = Brushes.LightBlue,
+                Stroke = Brushes.Black
+            };
+            Canvas.SetLeft(circle, x - circle.Width / 2);
+            Canvas.SetTop(circle, y - circle.Height / 2);
+            Canvas.Children.Add(circle);
+        }
+    }
+}
